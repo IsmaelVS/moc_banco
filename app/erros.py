23e939047cfc41b6a0032bc678c207d161b667c1
Ismaellov <ismael.venturadasilva0@gmail.com>
2019-05-23 07:01:00 -0300

    Adicionando tratativas de erro 401 e 404.

---
+++ app/erros.py
@@ -0,0 +1,13 @@
+from flask import Flask, abort, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/login')
+def login():
+    abort(401)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
