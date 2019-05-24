3fd59204501bd86245aa3d73a79067742dff099b
ismael.silva <ismael.silva@gastecnologia.com.br>
2019-05-24 16:41:09 -0300

    Alterando caminho da imagem do qr_code.

--- app/rotas/login.py
+++ app/rotas/login.py
@@ -19,7 +19,7 @@ def check_login():
     """Rota para validar dados do formulário."""
     # import ipdb; ipdb.sset_trace()
     if validar_login(request.form['nome'], request.form['senha']):
-        login_user(True)
+        # login_user(True)
         return render_template('qr_code.html', form=FormUsuario())
         # return redirect(url_for('login.login_template'))
     return render_template('login.html')
