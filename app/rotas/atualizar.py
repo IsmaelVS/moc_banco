0ec8f2836fd4d0e165709461cbe0f5844a4bcedf
ismael.silva <ismael.silva@gastecnologia.com.br>
2019-05-24 17:33:55 -0300

    Adicionando rota de alterar senha e de atualizar dados.

--- app/rotas/atualizar.py
+++ app/rotas/atualizar.py
@@ -1,13 +1,14 @@
 """Arquivo para rota de geração do Qrcode."""
 
 from flask import Blueprint, render_template
+from flask_login import current_user
 
 # from flask_login import login_required
 
 app = Blueprint('atualizar', __name__)
 
 
-@app.route('/',  methods=['GET'])
+@app.route('/')
 # @login_required
 def atualiza_usuario():
-    return render_template('atualiza.html')
+    return render_template('atualiza.html', usuario=current_user)
