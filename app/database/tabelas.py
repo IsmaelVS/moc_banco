e32704594427348b0f71dc56f4db2fcb6cdcedf0
ismael.silva <ismael.silva@gastecnologia.com.br>
2019-05-24 14:12:35 -0300

    Alterando URI do banco

--- app/database/tabelas.py
+++ app/database/tabelas.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'teste'
 
 # db_url = 'sqlite:///db.sqlite'
-db_url = 'postgres://pvgugjjzwjvpil:9c7adefda7aa2f5a678de9db6b484c82d37e2c88e5d77d8525315934cb9dc974@ec2-23-21-129-125.compute-1.amazonaws.com:5432/d3kvra84jha1nf'
+db_url = 'postgres://iyxswvnnweblgs:2ada92bca1b05b697a049be12696312ec6caa0b1452697205abd6ae6aaa3cf04@ec2-54-221-236-144.compute-1.amazonaws.com:5432/dcbj3ac2lobm87'
 
 db = SQLAlchemy()
 
