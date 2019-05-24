cbfa816222bd1546e0eb954056d1bf36715443ac
Ismaellov <ismael.venturadasilva0@gmail.com>
2019-05-24 04:17:09 -0300

    Melhorando função de geração de qr_code, e criação de função para envio de e-mail.

--- app/rotas/helpers/func.py
+++ app/rotas/helpers/func.py
@@ -1,11 +1,11 @@
 """Arquivo para funções utilizadas no projeto."""
 
+import smtplib
 from datetime import datetime
-
-from werkzeug.security import check_password_hash
 
 import pyqrcode
 from app.database.tabelas import Usuario, db
+from werkzeug.security import check_password_hash
 
 
 def validar_login(nome, senha):
@@ -20,11 +20,22 @@ def gerar_uuid(nome):
         nome=nome).first()
     data = datetime.now()
     uuid = str(data.year)[-2:] + '{:d}'.format(data.month).zfill(2)
-    n_id = 1 if 101 > user._id > 1 else 2 if 1001 > user._id > 100 else 3
+    n_id = 1 if 101 > user._id > 1 else 2 if 1001 > user._id > 100 else 3'{:d}'.format(data.month).zfill(9)
+    id = '{:d}'.format(user._id).zfill(9)
 
-    return uuid + n_id
+    return uuid + n_id + id
 
 
 def gerar_qrcode(uuid):
     code = pyqrcode.create('{}'.format(uuid))
     code.png('{}.png'.format(uuid), scale=6)
+
+
+def enviar_token(email):
+    content = 'Token para ativação da sua conta.'
+    mail = smtplib.SMTP('smtp.gmail.com', 587)
+    mail.ehlo()
+    mail.starttls()
+    mail.login('ismael.venturadasilva0@gmail.com', 'password')
+    mail.sendmail('ismael.ventiradasilva0@gmail.com',
+                  'ismael.venturadasilva0@gmail.com', content)
