f11ef013849d1b960f5d4f1a8a309a5879882bc5
Ismaellov <ismael.venturadasilva0@gmail.com>
2019-05-30 01:38:53 -0300

    Adicionando mensagens para o usuário caso faça uma ação não válida e limitando caracteres dos input.

--- app/rotas/cadastro_credenciado.py
+++ app/rotas/cadastro_credenciado.py
@@ -3,12 +3,11 @@
 
 from random import randint
 
+from app.database.tabelas import Usuario, db
+from app.rotas.helpers.func import checar_email_existente, enviar_token
 from flask import Blueprint, render_template, request
 from flask_login import logout_user
 from werkzeug.security import generate_password_hash
-
-from app.database.tabelas import Usuario, db
-from app.rotas.helpers.func import checar_email_existente, enviar_token
 
 app = Blueprint('cadastro-credenciado', __name__)
 
@@ -23,10 +22,10 @@ def home():
 @app.route('/checar', methods=['POST'])
 def checar_cadastro():
     """Rota inicial para checar cadastro de credenciado."""
-    nome = request.form['nome']
+    nome = request.form.get('nome').lower()
     hashed_senha = generate_password_hash(
-        request.form['senha'], method='sha256')
-    email = request.form['email']
+        request.form.get('senha'), method='sha256')
+    email = request.form.get('email')
     token = randint(10000, 99999)
     if checar_email_existente(email):
         return render_template('cadastro.html', user=True)
