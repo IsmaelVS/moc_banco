dae4dcfba5e8992d9bd3ab3617ba34633a85c417
Ismaellov <ismael.venturadasilva0@gmail.com>
2019-06-14 23:59:42 -0300

    Refatoração para adicionar WTForms e Bootstrap na rota de ativação de cadastro.

--- app/rotas/ativar_cadastro.py
+++ app/rotas/ativar_cadastro.py
@@ -5,7 +5,8 @@ from flask_login import logout_user
 
 from app import db
 from app.database.tabelas import Conta, Usuario
-from app.rotas.helpers.func import gerar_uuid
+from app.rotas.helpers.func import gerar_uuid, verificar_email_e_username
+from app.rotas.helpers.forms import FormAtivarCadastro
 from app.views.static.gerar_qr_code import gerar_qrcode
 
 app = Blueprint('ativar', __name__)
@@ -15,23 +16,27 @@ app = Blueprint('ativar', __name__)
 def home():
     """Rota para realizar ativação do cadastro."""
     logout_user()
-    return render_template('ativar_cadastro.html')
+    form = FormAtivarCadastro()
+    return render_template('ativar_cadastro.html', form=form)
 
 
 @app.route('/checar', methods=['POST'])
 def ativar_cadastro():
     """Rota para checar ativação do cadastro."""
-    token = request.form.get('token')
-    email = request.form.get('email').lower()
-    user = Usuario.query.filter_by(email=email).first()
+    form = FormAtivarCadastro(request.form)
+    if not form.validate_on_submit():
+        return render_template('ativar_cadastro.html', form=form)
+    user = verificar_email_e_username(form.username.data)
     if user:
-        if user.token == token:
+        if user.status == True:
+            return render_template('ativar_cadastro.html', form=form, ativado=True)
+        if user.token == form.token.data:
             user.status = True
-            n_conta = gerar_uuid(email)
+            n_conta = gerar_uuid(form.username.data)
             conta = Conta(usuario=user, saldo=0.0, conta=n_conta)
             db.session.add(conta)
             db.session.commit()
             gerar_qrcode(n_conta)
             return render_template('login.html')
-        return render_template('ativar_cadastro.html', token=True)
-    return render_template('ativar_cadastro.html', user=True)
+        return render_template('ativar_cadastro.html', form=form, token=True)
+    return render_template('ativar_cadastro.html', form=form, user=True)
