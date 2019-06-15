524076efbdf6b011721e67000b080ef5454fdd90
Ismaellov <ismael.venturadasilva0@gmail.com>
2019-06-14 23:53:00 -0300

    Removendo parâmetro form quando o cadastro é realizado com sucesso.

--- app/rotas/cadastro.py
+++ app/rotas/cadastro.py
@@ -22,8 +22,8 @@ def cadastro():
 @app.route('/checar', methods=['POST'])
 def checar_cadastro():
     """Rota para checar cadastro."""
+    logout_user()
     form = FormCadastro(request.form)
-
     if not form.validate_on_submit():
         return render_template('cadastro.html', form=form), 400
     if checar_usuario_existente(form.nome.data, form.email.data):
@@ -31,6 +31,5 @@ def checar_cadastro():
     token = criar_usuario(form.nome.data, form.username.data,
                           form.senha.data, form.email.data, 0)
     if enviar_token(form.email.data, token):
-        logout_user()
-        return render_template('ativar_cadastro.html', form=form)
+        return render_template('ativar_cadastro.html')
     return render_template('cadastro.html', form=form, falha_email=True), 400
